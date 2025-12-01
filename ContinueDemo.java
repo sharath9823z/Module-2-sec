c9eaf3bda6fda395ce8d10756d9e68b84f8f5c02
sharath9823z <sharath9823@gmail.com>
2025-12-01 10:06:59 +0530

    Create ContinueDemo.java

---
+++ ContinueDemo.java
@@ -0,0 +1,10 @@
+public class ReturnDemo {
+    public static void main(String[] args) {
+        System.out.println("Before return");
+
+        return;
+
+        // Code here will NOT run
+        // System.out.println("After return");
+    }
+}
